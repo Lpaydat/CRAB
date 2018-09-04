0644c358d14450f503356d17b1f8dc0cfdf1e00c
iMadooc <madooc.forex@gmail.com>
2018-09-05 04:42:28 +0700

    Add babel import related plugins

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,46 @@ module.exports = {
     '@babel/plugin-transform-react-inline-elements',
     '@babel/plugin-transform-react-jsx',
     'inline-react-svg',
+    [
+      'transform-imports',
+      {
+        'react-bootstrap': {
+          transform: member => `react-bootstrap/lib/${member}`,
+          preventFullImport: true,
+        },
+        lodash: {
+          transform: member => `lodash/${member}`,
+          preventFullImport: true,
+        },
+      },
+    ],
+    [
+      'babel-plugin-root-import',
+      {
+        paths: [
+          {
+            rootPathPrefix: '^',
+            rootPathSuffix: 'src',
+          },
+          {
+            rootPathPrefix: '#',
+            rootPathSuffix: 'src/data',
+          },
+          {
+            rootPathPrefix: '@',
+            rootPathSuffix: 'src/components',
+          },
+          {
+            rootPathPrefix: '$',
+            rootPathSuffix: 'src/mutations',
+          },
+          {
+            rootPathPrefix: '~',
+            rootPathSuffix: 'src/subscriptions',
+          },
+        ],
+      },
+    ],
   ],
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow'],
 };
