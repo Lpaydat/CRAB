c35edb758bafd9efc426b31df577359ab2f44541
iMadooc <madooc.forex@gmail.com>
2018-09-07 09:51:05 +0700

    Add Link component

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -6,18 +6,39 @@ const isLeftClickEvent = e => e.button === 0;
 const isModifiedEvent = e => !!(e.metaKey || e.altKey || e.ctrlKey || e.shiftKey);
 
 const Link = ({
-  to, children, onClick, ...props
-}: { to: string, children: React.Node, onClick: Function }) => {
+  to,
+  children,
+  onClick,
+  ...props
+}: {
+  to: string,
+  children: React.Node,
+  onClick?: Function
+}) => {
   const handleClick = (event) => {
-    if (onClick) { onClick(event); }
-    if (isModifiedEvent(event) || !isLeftClickEvent(event)) { return; }
-    if (event.defaultPrevented === true) { return; }
+    if (onClick) {
+      onClick(event);
+    }
+    if (isModifiedEvent(event) || !isLeftClickEvent(event)) {
+      return;
+    }
+    if (event.defaultPrevented === true) {
+      return;
+    }
 
     event.preventDefault();
     history.push(to);
   };
 
-  return <a href={to} {...props} onClick={handleClick}>{children}</a>;
+  return (
+    <a href={to} {...props} onClick={handleClick}>
+      {children}
+    </a>
+  );
+};
+
+Link.defaultProps = {
+  onClick: () => null,
 };
 
 export default Link;
